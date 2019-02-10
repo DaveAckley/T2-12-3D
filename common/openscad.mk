401108fc6275e5855031e8aabc5902219574190d
Dave Ackley <ackley@ackleyshack.com>
2019-02-09 22:49:15 -0700

    'Release candidate' building

--- common/openscad.mk
+++ common/openscad.mk
@@ -2,16 +2,24 @@ SRCFILES:=$(wildcard *.scad)
 BINDIR:=$(ROOTDIR)/output/stl
 FINAL_STL_FILES:=$(patsubst %.scad,$(BINDIR)/%.stl,$(SRCFILES))
 DRAFT_STL_FILES:=$(patsubst %.scad,$(BINDIR)/%-DRAFT.stl,$(SRCFILES))
+RC_STL_FILES:=$(patsubst %.scad,$(BINDIR)/%-RC.stl,$(SRCFILES))
 ifdef STL_FINAL
   STLFILES:=$(FINAL_STL_FILES)
 else
+ifdef STL_RC
+  STLFILES:=$(RC_STL_FILES)
+else
   STLFILES:=$(DRAFT_STL_FILES)
+endif
 endif
 ALLDEPS+=Makefile $(ROOTDIR)/common/openscad.mk
 EPOCH15:=$(shell expr `date +%s` / 1000)
 
 $(BINDIR)/%.stl:	%.scad $(ALLDEPS)
 	openscad -D BUILD_TIME_STAMP=\"$(EPOCH15)F\" -o $@ $< 
+
+$(BINDIR)/%-RC.stl:	%.scad $(ALLDEPS)
+	openscad -D BUILD_TIME_STAMP=\"$(EPOCH15)R\" -o $@ $< 
 
 $(BINDIR)/%-DRAFT.stl:	%.scad $(ALLDEPS)
 	openscad -D BUILD_TIME_STAMP=\"$(EPOCH15)D\" -o $@ $< 
